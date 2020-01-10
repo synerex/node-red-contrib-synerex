9550cc08083ae4b63aee97071d7b942498d279dc
Kazutoshi  Uetsuhara <kazutoshi.u@gmail.com>
2020-01-10 11:53:04 +0900

    add credential

--- synerex/fleet/fleet-subscribe-demand.js
+++ synerex/fleet/fleet-subscribe-demand.js
@@ -23,8 +23,21 @@ module.exports = function (RED) {
     var node = this
     var util = new Sxutil()
 
+    // Get credental
+    this.login = RED.nodes.getNode(config.login) // Retrieve the config node
+    if (!this.login) {
+      console.log('not login ??')
+      node.status({
+        fill: 'red',
+        shape: 'dot',
+        text: 'Credential error'
+      })
+      node.error('No credentials specified')
+      return
+    }
+
     const nodesvClient = new util.nodeapi.Node(
-      program.nodesrv,
+      this.login.nodesrv,
       grpc.credentials.createInsecure()
     )
     const NodeType = Protobuf.Enum.fromDescriptor(util.nodeapi.NodeType.type)
@@ -33,7 +46,7 @@ module.exports = function (RED) {
     // connecting server
     nodesvClient.RegisterNode(
       {
-        node_name: program.hostname,
+        node_name: this.login.hostname,
         node_type: NodeType.values.PROVIDER,
         channelTypes: [channel_RIDESHARE] // RIDE_SHARE
       },
