9abab2bef8f8c0dfccafaac4b9ce09450523a660
Kazutoshi  Uetsuhara <kazutoshi.u@gmail.com>
2020-01-13 19:27:13 +0900

    delete keepalive node

--- sxutil.js
+++ sxutil.js
@@ -314,33 +314,4 @@ module.exports = class Sxutil {
       console.log('Subscribe Done!')
     })
   }
-
-  startKeepAlive(nClient, resp) {
-    console.log('startKeepAlive')
-    console.log('resp.secret', resp.secret)
-    if (!this.keepaliveIterval) {
-      global.update = 0
-      this.keepaliveIterval = setInterval(() => {
-        var updt = {
-          node_id: resp.node_id,
-          secret: resp.secret,
-          update_count: global.udpate++,
-          node_status: 0,
-          node_arg: 'OK'
-        }
-        nClient.KeepAlive(updt, (err, resp) => {
-          if (!err) {
-            console.log('KeepAlive OK', resp)
-          } else {
-            console.log('Error!', err)
-          }
-        })
-      }, resp.keepalive_duration * 1000)
-    }
-  }
-
-  stopKeepAlive() {
-    console.log('KP STOP!!!!!!!!!!!!!')
-    clearInterval(this.keepaliveIterval)
-  }
 }
